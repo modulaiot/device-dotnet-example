de0401a86552f9db61b3f883c435d99e5c1e170d
Timothy Purchas <timothy@TPF.local>
2020-03-22 00:20:06 +1100

    init device

--- device/Program.cs
+++ device/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using ModulaIOT.Device;
 
 namespace device
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var root = new RootCommand("Dotnet Modula IOT Device");
+
+            var run = new Command("run", "Run the device");
+            run.Handler = CommandHandler.Create(async () =>
+            {
+                Console.WriteLine("Run");
+                var device = new ModulaIOTDeviceBuilder().Build();
+                await device.Run();
+                return 0;
+            });
+            root.AddCommand(run);
+
+            root.Handler = CommandHandler.Create(async () =>
+            {
+                Console.WriteLine("Executing Default Command, Run..");
+                return await run.InvokeAsync(args);
+            });
+
+            return await root.InvokeAsync(args);
         }
     }
 }
